37a465127f7f444a597bccee4c1fc5d6d54cac48
João Vítor Carli <jvitor.carli@tutamail.com>
2021-05-17 09:14:15 -0300

    Make shellcheck static analysis tool for shell scripts work

--- lua/efm/shellcheck.lua
+++ lua/efm/shellcheck.lua
@@ -1,7 +1,14 @@
--- not working
+-- shellcheck linter
+-- config bellow not working, why?
+-- return {
+--     lintCommand = "shellcheck -f gcc -x -",
+--     lintStdin = true,
+--     lintFormats = {"%f=%l:%c: %trror: %m", "%f=%l:%c: %tarning: %m", "%f=%l:%c: %tote: %m"},
+--     lintSource = "shellcheck"
+-- }
+
+-- working
 return {
-    lintCommand = "shellcheck -f gcc -x -",
-    lintStdin = true,
-    lintFormats = {"%f=%l:%c: %trror: %m", "%f=%l:%c: %tarning: %m", "%f=%l:%c: %tote: %m"},
-    lintSource = "shellcheck"
+    LintCommand = 'shellcheck -f gcc -x',
+    lintFormats = {'%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m', '%f:%l:%c: %tote: %m'}
 }
