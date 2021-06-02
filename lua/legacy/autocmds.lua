abfe7c5064ef9e53aa98811b056b0e452f27bf51
João Vítor Carli <jvitor.carli@tutamail.com>
2021-06-02 15:26:14 -0300

    [INTERNAL] Remove filetypes autocmds in favor of ftplugin overrides

--- lua/legacy/autocmds.lua
+++ lua/legacy/autocmds.lua
@@ -6,22 +6,6 @@
 -- but is being worked on, see: https://github.com/neovim/neovim/pull/12378
 
 -- =======================================--
---     Tabs and spaces configuration      --
--- =======================================--
-
--- Golang
--- Typescript JSX
-vim.cmd[[autocmd FileType typescriptreact setlocal softtabstop=2 shiftwidth=2 expandtab]]
--- Typescrpt 
-vim.cmd[[autocmd FileType typescript setlocal softtabstop=2 shiftwidth=2 expandtab]]
--- Javascript
-vim.cmd[[autocmd FileType javascript setlocal softtabstop=2 shiftwidth=2 expandtab]]
--- Html
-vim.cmd[[autocmd FileType html setlocal softtabstop=2 shiftwidth=2 expandtab]]
--- Svelte
-vim.cmd[[autocmd FileType svelte setlocal softtabstop=2 shiftwidth=2 expandtab]]
-
--- =======================================--
 --                 Packer                 --
 -- =======================================--
 
