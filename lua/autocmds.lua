90792ea54d64acf0f61bbe58d5dc6ccfd5659526
João Vítor Carli <jvitor.carli@tutamail.com>
2021-06-03 11:15:27 -0300

    [REFACTOR] Refactor SmartNumberToggle autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -14,11 +14,13 @@
 -- Maintainer:        <https://jeffkreeftmeijer.com>
 -- Version:           2.1.2
 
-vim.cmd [[augroup smartnumbertoggle ]]
-    vim.cmd [[autocmd!]]
-    vim.cmd [[autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif]]
-    vim.cmd [[autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif]]
-vim.cmd [[augroup end ]]
+vim.api.nvim_exec([[
+    augroup SmartNumberToggle
+        autocmd!
+        autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+        autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+    augroup end
+]], false)
 
 -- =======================================--
 --                 Yank                   --
