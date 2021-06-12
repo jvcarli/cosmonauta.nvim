51c09683c0a6dabeb43f49b997716faa34da5264
João Vítor Carli <jvitor.carli@tutamail.com>
2021-06-12 13:22:17 -0300

    [FEATURE] Update brew.sh script

--- lsp-updater/brew.sh
+++ lsp-updater/brew.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-brew upgrade efm-langserver
-brew upgrade texlab
+echo "Installing or upgrading language servers managed by macOS Homebrew..."
+
+brew_installed_ls=("efm-langserver" "texlab")
+
+for server in "${brew_installed_ls[@]}"; do
+	brew upgrade "$server"
+done
