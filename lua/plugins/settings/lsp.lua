11678786efe43e4320d678ab3bebd908880ede95
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-02-02 15:59:09 -0300

    chore: refactor custom lsp on_attach function with clearer comments and to use the new vim.keymap.set api

--- lua/plugins/settings/lsp.lua
+++ lua/plugins/settings/lsp.lua
@@ -7,51 +7,56 @@
 
 local lspconfig = require "lspconfig"
 
--- {{{ on_attach
-local on_attach = function(client, bufnr)
-  require("lsp_signature").on_attach() -- NOTE: add in lsp client
-  require("illuminate").on_attach(client) -- NOTE: add in lsp client
+-- {{{ Custom Lsp Attach
+local custom_lsp_attach = function(client, bufnr)
+  -- Uses LSP as the handler for Neovim built-in omnifunc (i_CTRL-X_CTRL-O)
+  -- DISABLED in favor of nvim_cmp autocompletion plugin
+  -- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Mappings
+  -- These mappings only work inside a buffer with a LSP attached
+  -- which is good because it won't affect default mappings when there isn't a LSP
 
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "<leader>wl",
-    "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-    opts
-  )
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "<leader>so",
-    [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]],
-    opts
-  )
+  local opts = { remap = false, silent = true, buffer = bufnr }
+  -- TODO: `buffer = bufnr` and `buffer = 0` seems in practice to mean the same thing
+  -- find out if there's any difference
+  -- buffer: means buffer handle
+  -- buffer = bufnr makes it so the mapping will apply only for the buffer where there's a lsp attached
 
-  -- if the language server has formatting capabilities format the buffer
-  if client.resolved_capabilities.document_formatting then
-    -- vim.lsp.buf.formatting() -- asynchronous formatting
-    -- vim.lsp.buf.formatting_sync() -- synchronous formatting, better to avoid desync problems
-    vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
-  end
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+  -- Hover and help
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+  -- Workspace management
+  -- TODO: explore lsp workspace management and how it works
+  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set("n", "<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("v", "<leader>ca", vim.lsp.buf.range_code_action, opts)
+
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.lsp.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.lsp.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+
+  vim.keymap.set("n", "<leader>so", function()
+    return require("telescope.builtin").lsp_document_symbols()
+  end, opts)
+
+  -- Thirdy-party LSP addons
+  require("lsp_signature").on_attach()
+  require("illuminate").on_attach(client)
 end
 -- }}}
 
