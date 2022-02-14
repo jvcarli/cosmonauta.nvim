571e509f2690eb9ba90c9fefa63d8546de626392
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-02-14 13:19:46 -0300

    chore: remove tabby.nvim highlight configuration

--- plugin/extra_colorscheme.lua
+++ plugin/extra_colorscheme.lua
@@ -9,11 +9,4 @@ if vim.g.colors_name == "zenbones" then
 
   -- rhysd/clever-f.vim
   vim.cmd "highlight CleverFDefaultLabel cterm=bold ctermfg=9 gui=bold guifg=#A8334C"
-
-  -- nanozuki/tabby.nvim
-  -- vim.cmd "highlight TabLine cterm=underline ctermfg=0 ctermbg=7 gui=italic guifg=#2C363C guibg=#c4b6af"
-
-  -- BUG: tabby highlight colors are defined wrong
-  -- vim.cmd "highlight TabLineFill cterm=reverse gui=reverse guifg=#FF0000 guibg=#FF0000"
-  -- vim.cmd "highlight TabLineSel cterm=bold gui=bold guifg=#FF0000 guibg=#FF0000"
 end
