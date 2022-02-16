8f23bb79ab335d3b5ae13e49ccbf436a086e80a0
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-02-16 09:42:35 -0300

    chore: organize null-ls.nvim builtins

--- lua/plugins/settings/null-ls.lua
+++ lua/plugins/settings/null-ls.lua
@@ -1,43 +1,60 @@
-require("null-ls").setup {
-  sources = {
-    require("null-ls").builtins.diagnostics.vint, -- viml
-    require("null-ls").builtins.diagnostics.codespell,
-    require("null-ls").builtins.diagnostics.vale, -- TODO: add proselint rules emulated through vale, see https://github.com/jose-elias-alvarez/null-ls.nvim/pull/241
-    require("null-ls").builtins.diagnostics.selene,
-    require("null-ls").builtins.diagnostics.shellcheck,
-    -- require("null-ls").builtins.diagnostics.pkgbuildlint, -- TODO: make pkgbuild shellcheck lint work
-    require("null-ls").builtins.diagnostics.flake8,
+local null_ls = require "null-ls"
 
-    require("null-ls").builtins.formatting.rustywind,
-    require("null-ls").builtins.formatting.stylua,
-    -- require("null-ls").builtins.formatting.shellharden, -- BUG: something is wrong with its formatting
+local null_ls_sources = {
 
-    require("null-ls").builtins.formatting.shfmt.with {
-      extra_args = {
-        "-i", -- uses n spaces instead of the tab default
-        vim.bo.shiftwidth, -- uses shiftwidth as arg to -i,
-        "-ci", -- switch cases will be indented
-        "-s", -- simplify the code
-        "-bn", -- binary ops like && and | may start a line
-      },
-    },
+  -- TODO: restrict null-ls to specific cases, so git history won't be all messed up
+  -- in repos you want to contribute but don't own or when they don't follow the same style guide as you.
+  -- see: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#conditional-sources
 
-    require("null-ls").builtins.formatting.prettierd.with {
-      filetypes = {
-        "html",
-        "json",
-        "yaml",
-        "markdown",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-      },
-    },
+  null_ls.builtins.diagnostics.vint, -- viml
+  null_ls.builtins.diagnostics.codespell,
+  null_ls.builtins.diagnostics.vale, -- TODO: add proselint rules emulated through vale, see https://github.com/jose-elias-alvarez/null-ls.nvim/pull/241
+  null_ls.builtins.diagnostics.selene,
+  null_ls.builtins.diagnostics.shellcheck,
+  null_ls.builtins.diagnostics.flake8,
 
-    require("null-ls").builtins.code_actions.gitsigns,
-
-    require("null-ls").builtins.formatting.codespell,
-    -- require("null-ls").builtins.diagnostics.tidy, -- TODO: add support for it
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.shellharden,
+  null_ls.builtins.formatting.black.with {
+    condition = function(utils)
+      -- see: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Source-specific-Configuration#specifying-edition for a example
+      -- see: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#conditional-sources
+      --  TODO: only format when black package is present in the project
+      -- by parsing a pyproject.toml, setup.py, or requirements.txt file
+      return utils.root_has_file { "pyproject.toml" }
+    end,
   },
+  null_ls.builtins.formatting.shfmt.with {
+    extra_args = {
+      "-i", -- uses n spaces instead of the tab default
+      vim.bo.shiftwidth, -- uses shiftwidth as arg to -i,
+      "-ci", -- switch cases will be indented
+      "-s", -- simplify the code
+      "-bn", -- binary ops like && and | may start a line
+    },
+  },
+
+  null_ls.builtins.formatting.prettierd.with {
+    -- If you have locally installed prettier in your package, it will use that.
+    -- Otherwise, it will use the one bundled with prettierd itself.
+    filetypes = {
+      "html",
+      "json",
+      "yaml",
+      "markdown",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+  },
+
+  null_ls.builtins.code_actions.gitsigns,
+
+  -- null_ls.builtins.diagnostics.pkgbuildlint, -- arch pkgbuildlint  TODO: add support for it
+  -- null_ls.builtins.diagnostics.tidy, -- html tidy TODO: add support for it
+}
+
+null_ls.setup {
+  sources = null_ls_sources,
 }
