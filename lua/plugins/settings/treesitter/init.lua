321c9f5bca4d8c05605ab9c30f465cdd17ee1b6b
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-03-24 14:40:35 -0300

    feat: add Golang templates tree-sitter support

--- lua/plugins/settings/treesitter/init.lua
+++ lua/plugins/settings/treesitter/init.lua
@@ -1,4 +1,21 @@
 -- vim:fileencoding=utf-8:ft=lua:foldmethod=marker
+
+-- Golang templates grammar for tree-sitter
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = { "src/parser.c" },
+  },
+  filetype = "gotmpl",
+  used_by = {
+    "gohtmltmpl",
+    "gotexttmpl",
+    "gotmpl",
+    "yaml", -- see: https://github.com/ngalaiko/tree-sitter-go-template/issues/1
+  },
+}
+
 require("nvim-treesitter.configs").setup {
   ensure_installed = {
     "bash",
