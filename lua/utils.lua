b0771d7af180ad7ee7f0b49f1bbd642fd26249bf
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-03-29 13:03:49 -0300

    feat(utils) add basename function

--- lua/utils.lua
+++ lua/utils.lua
@@ -19,12 +19,21 @@ M.file_exists = function(...)
   -- see: https://stackoverflow.com/questions/7573588/variable-number-of-function-arguments-lua-5-1
   local files_to_check = { ... }
   for _, file in pairs(files_to_check) do
+    -- TODO: include tilde expansion
     -- see: https://stackoverflow.com/questions/3098521/how-to-detect-if-a-specific-file-exists-in-vimscript
     if vim.fn.filereadable(file) ~= 1 then
       return nil -- is returning nil the same as just `return`?
     end
   end
   return true
+end
+
+--- Function equivalent to basename in POSIX systems
+--- taken from: https://github.com/Donearm/scripts/blob/master/lib/basename.lua
+--@param str the path string
+M.basename = function(str)
+  local name = string.gsub(str, "(.*/)(.*)", "%2")
+  return name
 end
 
 return M
