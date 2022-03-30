f5190945b6d1b1e879a3b4f568afb773829e4d48
Joao Vitor Carli Pereira <jvitor.carli@tutamail.com>
2022-03-30 15:39:02 -0300

    feat: update indent-blankline settings

--- lua/plugins/settings/indent_blankline.lua
+++ lua/plugins/settings/indent_blankline.lua
@@ -2,22 +2,62 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
 require("indent_blankline").setup {
-  char = "┊",
-  -- char = "│",
+  char = "│",
+  -- char_list = { "|", "¦", "┆", "┊" }, -- Too noisy
+
+  context_char = "┃",
+  -- context_char_blankline = "┃",
+
+  -- char_blankline = "⤦", -- Too noisy
+  -- char_list_blankline = { "|", "¦", "┆", "┊" }, -- Too noisy
+
+  -- space_char_blankline = "⤦", -- Too noisy, but can be useful
 
   buftype_exclude = {
     "terminal",
     "nofile",
   },
+
   filetype_exclude = {
     "help",
     "packer",
     "startify",
     "man",
     "gitmessengerpopup",
+    "dirbuf",
+    "dirvish",
   },
-  show_trailing_blankline_indent = false,
-}
 
--- TODO: convert to lua, how does v:true translate to lua?
-vim.cmd "let g:indent_blankline_use_treesitter = v:true"
+  -- IDE like context display
+  -- column char gets highlighted
+  show_current_context = true,
+
+  -- DON'T show current context start,
+  -- that means don't use highlight groups for it!
+  -- affects the LINE (be a function, a class, ...)
+  -- that starts the indent. I found this too noisy.
+  show_current_context_start = false,
+
+  -- I find it misleading when set to true
+  show_trailing_blankline_indent = false,
+
+  -- The maximum indent level increase from line to line.
+  -- Set this option to 1 to make aligned trailing multiline comments not
+  -- create indentation.
+  max_indent_increase = 1,
+
+  -- BUG: This option is UNUSABLE with python, but is due its
+  -- treesitter implementation, not indent_blankline itself.
+  -- see: https://github.com/lukas-reineke/indent-blankline.nvim/issues/226
+  --
+  -- TODO: turn off for python only!
+  -- TODO: convert to lua, how does v:true translate to lua?
+  use_treesitter = false,
+
+  -- Import option for highlighting indents which its start is not displayed
+  -- in the buffer anymore, e.g.: long <div> in an html file.
+  -- see: https://github.com/lukas-reineke/indent-blankline.nvim/issues/374
+  --   NOTE: high numbers cause some lag in the editor
+  -- TODO: find a suitable number
+  viewport_buffer = 40, -- defaults to 10
+}
