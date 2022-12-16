9ce119f6c0e027b4e36c18a58affda634648c5e8
Joao Vitor Carli <jvitor.carli@gmail.com>
2022-12-16 18:29:32 -0300

    chore: update local tex ftplugin

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -4,5 +4,14 @@ vim.g.tex_conceal = "abdmg" -- Neovim builtin option
 
 vim.g.tex_flavor = "latex" -- Neovim builtin option
 
--- TODO: test tex conceallevel
-vim.wo.conceallevel = 1
+-- NOTE: concealing can be confusing
+vim.wo.conceallevel = 0
+
+-- SEE: https://tex.stackexchange.com/questions/349/what-is-the-practical-difference-between-latex-and-pdflatex
+
+vim.cmd [[setlocal spelllang=pt]] -- pt <3
+
+-- Enable default vim spelling
+-- vim.opt.spell = true -- TODO: I want to enable spelling only for tex buffers
+--                               But with vim.opt it enables spelling for EVERYTHING which is really annoying
+vim.cmd [[setlocal spell]]
