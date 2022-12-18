9a9b2714cdc8c12dc297b252fce27aa36c5d37f8
Joao Vitor Carli <jvitor.carli@gmail.com>
2022-12-18 09:53:47 -0300

    chore: simplify comment.nvim config

--- lua/plugins/settings/comment_nvim.lua
+++ lua/plugins/settings/comment_nvim.lua
@@ -1,31 +1,3 @@
 require("Comment").setup {
-  -- nvim-ts-context-commentstring integration (the recommended way)
-  ---@param ctx Ctx
-  pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
-    if
-      vim.bo.filetype == "typescriptreact"
-      or vim.bo.filetype == "javascriptreact"
-      or vim.bo.filetype == "svelte"
-      or vim.bo.filetype == "html"
-    then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end,
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 }
