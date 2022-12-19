07a100b7386dcc2d28295fe38ab29c0f6156c9e8
Joao Vitor Carli <jvitor.carli@gmail.com>
2022-12-19 10:44:06 -0300

    feat: add bootstrap scaffold

---
+++ lua/user/modules/bootstrap.lua
@@ -0,0 +1,64 @@
+local M = {}
+local is_mac = require("user.modules.utils").is_mac
+local is_linux = require("user.modules.utils").is_linux
+
+M.base_pkg = {
+  "git",
+  "lua-language-server",
+  "shfmt",
+  "vscode-html-language-server",
+  "texlab",
+  "xdotool", -- vimtex.vim plugin, TODO: is it really needed, i don't think it is
+  -- "python-lsp-server[all]", -- pylsp (community fork), NOT pyls_ms (microsoft one)
+  -- "jedi-language-server",
+}
+
+M.linux_pkg = function()
+  -- Arch linux easy detection
+  if vim.fn.executable "pacman" == 1 then
+    return {
+      "selene-linter",
+      "xsel", -- paste.vim and for (linux clipboard?)
+    }
+  end
+end
+
+M.macos_pkg = {
+  "selene",
+}
+
+M.is_executable = function(base_pkg)
+  for _, value in pairs(base_pkg) do
+    if vim.fn.executable(value) ~= 1 then
+      vim.notify(value .. [[ executable wasn't found or is not in the $PATH!]])
+    end
+  end
+end
+
+M.append_to_table = function(to, from)
+  for _, value in pairs(from) do
+    table.insert(to, value)
+  end
+end
+
+-- TODO: Add complete bootstrap
+--       It should resolve itself, no matter if OS is a Linux distro, macos or Windows.
+M.bootstrap_run = function()
+  if is_mac then
+    M.append_to_table(M.base_pkg, M.macos_pkg) -- include macos only pkgs in default pkgs
+    M.is_executable(M.base_pkg)
+    return
+  elseif is_linux then
+    local linux_pkg = M.linux_pkg()
+    if linux_pkg ~= nil then
+      M.append_to_table(M.base_pkg, linux_pkg) -- include linux only pkgs in default pkgs
+      M.is_executable(M.base_pkg)
+      return
+    end
+    vim.notify "You will need to manually install the required packages for your distro!"
+    return
+  end
+  -- TODO: write Windows bootstrapping logic
+end
+
+return M.bootstrap_run()
