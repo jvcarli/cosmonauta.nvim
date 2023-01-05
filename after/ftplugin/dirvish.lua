1d1b9aa3de2cc60f9149b32a0378176132a9bb91
Joao Vitor Carli <jvitor.carli@gmail.com>
2023-01-05 08:09:55 -0300

    refactor: make norelativenumber work with dirvish.vim
    For some reason setting this option using lua is not working
    properly, so I use vimscript instead.

--- after/ftplugin/dirvish.lua
+++ after/ftplugin/dirvish.lua
@@ -1,1 +1,3 @@
-vim.opt.relativenumber = false
+-- Only on dirvish windows because otherwise it will disable relativenumber for other filetypes
+-- TODO: convert to lua
+vim.cmd [[setlocal norelativenumber]]
