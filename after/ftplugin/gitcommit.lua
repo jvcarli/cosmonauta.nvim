017dcc57e0bb63cebf1ff04c639d0cfefa524baa
Joao Vitor Carli <jvitor.carli@gmail.com>
2023-01-05 08:14:28 -0300

    chore: update gitcommit ftplugin

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,3 +1,8 @@
 -- Auto fold diffs in commit
 -- Reasoning, SEE: https://github.com/tpope/vim-fugitive/issues/146#issuecomment-3733461
-vim.wo.foldmethod = "syntax"
+
+-- REALLY slow on insert mode even with FastFold plugin
+-- so I'm using tree-sitter highlights and waiting for folding for gitcommit files
+-- vim.wo.foldmethod = "syntax"
+
+vim.wo.spell = true
