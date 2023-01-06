ac1211b7afc885e8c0d6f582e295282a50518ab1
Joao Vitor Carli <jvitor.carli@gmail.com>
2023-01-06 00:22:41 -0300

    chore(plugins): update gitsigns config

--- lua/plugins/settings/gitsigns.lua
+++ lua/plugins/settings/gitsigns.lua
@@ -1,31 +1,35 @@
+local gitsigns_mappings = require("keymaps").gitsigns_mappings
+
 require("gitsigns").setup {
   signs = {
 
-    -- Left Half Block Char -> ▌ <- (U+258C)
-    -- see: https://www.compart.com/en/unicode/U+258C
+    -- U+258C: Left Half Block Char -> ▌ <-
+    -- U+258E: Left one quarter block char --> ▎<-
+    -- U+258B: Left Five Eighths Block char -> ▋ <-
+    -- U+258A: Left three quarters block -->▊<-
 
-    -- Left Five Eighths Block char -> ▋ <- (U+258B)
-    -- see: https://www.compart.com/en/unicode/U+258B
+    -- U+2590: Right Half Block char ->▐ <-
+    add = { text = "▐" },
+    change = { text = "▐" },
+    delete = { text = "▐" },
+    topdelete = { text = "▐" },
+    changedelete = { text = "▐" },
 
-    -- Right Half Block char ->▐ <-
-    -- see: https://www.compart.com/en/unicode/U+2590
-
-    -- left one quarter block char --> ▎<- (U+258E)
-    -- see: https://www.compart.com/en/unicode/U+258E
-
-    -- left three quarters block -->▊-< (U+258A)
-
-    add = { hl = "GitSignsAdd", text = "▊", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▊", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "▊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "▊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▊", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    -- add = { text = "+" },
+    -- change = { text = "~" },
+    -- delete = { text = "_" },
+    -- topdelete = { text = "‾" },
+    -- changedelete = { text = "~" },
   },
 
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true,
+  },
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
@@ -36,8 +40,15 @@ require("gitsigns").setup {
     relative_time = false,
   },
 
-  -- sign_priority = 1, -- the lower the number the higher the priority
+  -- WARN: when the file is too large `attach_to_untracked = true` causes slowness
+  attach_to_untracked = true,
+
+  sign_priority = 1, -- the lower the number the higher the priority, appear first ALWAYS
+  -- TODO: test sign_priority
   yadm = {
     enable = false,
   },
+  on_attach = function(bufnr)
+    gitsigns_mappings(bufnr)
+  end,
 }
