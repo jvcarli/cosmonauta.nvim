001385ab59b5bde6a4bd5765c5772e2ad846f8e6
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2024-05-05 14:54:53 -0300

    refactor: move dap_todos.lua content

--- lua/dap_todos.lua
+++
@@ -1,9 +0,0 @@
--- How to consume Debug Adapter / debugServerMain.ts outside of VSCode?
--- SEE: https://github.com/microsoft/vscode-js-debug/issues/926
--- SEE: https://github.com/mxsdev/nvim-dap-vscode-js
-
--- https://www.reddit.com/r/neovim/comments/1051iww/nextjs_dap_config/
--- https://github.com/nikfp/nvim-config/blob/main/lua/nikp/dap/init.lua
--- https://github.com/anasrar/.dotfiles/blob/671c32cbf44d828e5b5128ada4f4637b58a0b792/neovim/.config/nvim/lua/rin/DAP/languages/typescript.lua
-
--- https://harrisoncramer.me/debugging-in-neovim/
--- lua/user/plugins/settings/dap/init.lua
+++ lua/user/plugins/settings/dap/init.lua
@@ -15,3 +15,13 @@
 -- TODO: https://alpha2phi.medium.com/remote-code-development-on-termux-using-vs-code-and-neovim-ca9fd13b7824
 -- TODO: https://alpha2phi.medium.com/neovim-lsp-and-dap-using-lua-3fb24610ac9f
 -- TODO: https://alpha2phi.medium.com/setting-up-neovim-for-rust-debugging-termdebug-and-vimspector-df749e1ba47c
+
+-- How to consume Debug Adapter / debugServerMain.ts outside of VSCode?
+-- SEE: https://github.com/microsoft/vscode-js-debug/issues/926
+-- SEE: https://github.com/mxsdev/nvim-dap-vscode-js
+
+-- https://www.reddit.com/r/neovim/comments/1051iww/nextjs_dap_config/
+-- https://github.com/nikfp/nvim-config/blob/main/lua/nikp/dap/init.lua
+-- https://github.com/anasrar/.dotfiles/blob/671c32cbf44d828e5b5128ada4f4637b58a0b792/neovim/.config/nvim/lua/rin/DAP/languages/typescript.lua
+
+-- https://harrisoncramer.me/debugging-in-neovim/
