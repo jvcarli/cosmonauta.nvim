7ae882cc7c03c3687cab55fb3fb9aebc6f1811b3
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2024-05-05 16:19:47 -0300

    refactor!: simplify testing workflow
    Do not depend on packer.nvim or any other package manager.

--- test/init.lua
+++ test/init.lua
@@ -1,78 +1,17 @@
--- TODO: this could lead to a testing plugin
--- SEE: part is taken from https://github.com/neovim/nvim-lspconfig/blob/master/test/minimal_init.lua
+-- Test init file
+-- Inspired by minimal_init.lua
+-- SEE: https://github.com/neovim/nvim-lspconfig/blob/master/test/minimal_init.lua
 
--- Neovim defaults:
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.cmd [[colorscheme default]]
+-- WARN: For all purposes ~/.config/nvim/test will behave like ~/.config/nvim
+--       ~/.config/nvim/test/lua will be set to be the lua runtime path
+--       for Neovim instead of the default ~/.config/nvim/lua.
+vim.opt.runtimepath:prepend("~/.config/nvim/test")
 
-local on_windows = vim.loop.os_uname().version:match "Windows"
+-- NOTE: if we include ~/.config/nvim we'll load
+--       ftplugins, plugins, etc...
+-- TODO: find a lua version for this command.
+--       For some reason vim.opt.runtimepath:remove("~/.config/nvim") doesn't work
+vim.cmd([[set runtimepath-=~/.config/nvim]])
 
-local function join_paths(...)
-  local path_sep = on_windows and "\\" or "/"
-  local result = table.concat({ ... }, path_sep)
-  return result
-end
-
--- Adjust runtimepath
-vim.cmd [[set runtimepath=~/.config/nvim/test]]
-vim.cmd [[set runtimepath+=$VIMRUNTIME]]
-
-local temp_dir = vim.loop.os_getenv "TEMP" or "/tmp"
-
-vim.cmd("set packpath=" .. join_paths(temp_dir, "nvim", "site"))
-
-local package_root = join_paths(temp_dir, "nvim", "site", "pack")
-local install_path = join_paths(package_root, "packer", "start", "packer.nvim")
-local compile_path = join_paths(install_path, "plugin", "packer_compiled.lua")
-
--- Define plugins
-local test_plugins_status_ok, test_plugins = pcall(dofile, vim.env.HOME .. "/.config/nvim/test/test_plugins.lua")
---
--- NOTE: Packer.nvim will always be needed
-if not test_plugins_status_ok then
-  -- No plugin list to test yet, but packer will be needed anyway
-  test_plugins = { "wbthomason/packer.nvim" }
-
-  print [[WARN: You have not defined a plugin list to test. Please write one, returning it as a lua table at "~/.config/nvim/test/test_plugins.lua".]]
-else
-  -- append packer to the plugins you want to test so you won't have to include it there
-  table.insert(test_plugins, "wbthomason/packer.nvim")
-end
-
-local function load_plugins()
-  require("packer").startup {
-    test_plugins,
-    config = {
-      package_root = package_root,
-      compile_path = compile_path,
-    },
-  }
-end
-
-local load_config = function()
-  local config_to_test_status_ok, test_config = pcall(dofile, vim.env.HOME .. "/.config/nvim/test/test_config.lua")
-
-  -- NOTE: Usually configurating something is needed but not always
-  if not config_to_test_status_ok then
-    print [[WARN: You have not defined a config file to test. If this isn't your intention write one at "~/.config/nvim/test/test_config.lua".]]
-  else
-    return test_config
-  end
-end
-
--- Packer auto install
-if vim.fn.isdirectory(install_path) == 0 then
-  vim.fn.system { "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path }
-  load_plugins()
-  require("packer").sync()
-  local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-  vim.api.nvim_create_autocmd(
-    "User",
-    { pattern = "PackerComplete", callback = load_config, group = packer_group, once = true }
-  )
-else
-  load_plugins()
-  require("packer").sync()
-  load_config()
-end
+-- Load any desired module
+-- require("<module_name>")
