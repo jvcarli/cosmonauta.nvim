5ea9e0b723f4bf1e77539a47245b47489f5d5c11
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2024-05-10 11:44:10 -0300

    feat: update testing workflow

--- test/init.lua
+++ test/init.lua
@@ -1,17 +1,28 @@
 -- Test init file
 -- Inspired by minimal_init.lua
 -- SEE: https://github.com/neovim/nvim-lspconfig/blob/master/test/minimal_init.lua
+-- WARN: this file is used to set up the correct runtimepath for testing, and
+--       shouldn't be used directly. Use ./test.lua as the regular init.lua.
+--       If it doesn't exist create one.
+--       This is done so one can freely change files and directories for testing
+--       without cluterring git status.
 
--- WARN: For all purposes ~/.config/nvim/test will behave like ~/.config/nvim
---       ~/.config/nvim/test/lua will be set to be the lua runtime path
+-- WARN: For all purposes ~/.config/nvim/test will behave like ~/.config/nvim,
+--       so ~/.config/nvim/test/lua will be set to be the lua runtime path
 --       for Neovim instead of the default ~/.config/nvim/lua.
 vim.opt.runtimepath:prepend("~/.config/nvim/test")
 
 -- NOTE: if we include ~/.config/nvim we'll load
---       ftplugins, plugins, etc...
+--       ftplugins, plugins, etc... from its standards locations.
+--       We don't want that.
 -- TODO: find a lua version for this command.
 --       For some reason vim.opt.runtimepath:remove("~/.config/nvim") doesn't work
 vim.cmd([[set runtimepath-=~/.config/nvim]])
 
--- Load any desired module
--- require("<module_name>")
+local test_file = vim.fn.stdpath("config") .. "/test/test.lua"
+
+if vim.fn.filereadable(test_file) == 1 then
+  dofile(test_file)
+else
+  vim.print("Please place a test file at ~/.config/nvim/test/test.lua")
+end
